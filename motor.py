ef44103f94134a4b7cb5637cdc1cff29e1612f1e
Tamas Halasi <trusted.tomato@gmail.com>
2022-10-28 21:20:32 +0200

    temp

--- motor.py
+++ motor.py
@@ -6,32 +6,27 @@ class Motor:
         self,
         speed_pin: int,
         direction_pin: int,
-        default_duty: float = 0
+        default_velocity: float = 0
     ):
         self._speed_pin = speed_pin
         self._direction_pin = direction_pin
-        self._duty = default_duty
-        self._direction = False
+        self._velocity = default_velocity
 
         GPIO.setup(speed_pin, GPIO.OUT)
         GPIO.setup(direction_pin, GPIO.OUT)
 
     def set_velocity(self, velocity: float):
-        if velocity > 0:
-            self._duty = 1 - velocity
-            self._direction = False
-        else:
-            self._duty = 1 + velocity
-            self._direction = True
+        self._velocity = velocity
 
     async def start_running(self):
         # run motors until GPIO is cleaned up
         try:
             while True:
-                GPIO.output(self._direction_pin, self._direction)
+                GPIO.output(self._direction_pin, True if self._velocity > 0 else False)
+
                 GPIO.output(self._speed_pin, True)
-                await asyncio.sleep(0.02 * self._duty)
+                await asyncio.sleep(0.02 * self._velocity)
                 GPIO.output(self._speed_pin, False)
-                await asyncio.sleep(0.02 * (1 - self._duty))
+                await asyncio.sleep(0.02 * (1 - self._velocity))
         except (RuntimeError):
             pass
