8926b4234c79b11fc44218e1ee9c4db230eb4355
Tamas Halasi <trusted.tomato@gmail.com>
2022-10-28 21:04:34 +0200

    temp

--- car.py
+++ car.py
@@ -6,23 +6,25 @@ class Car:
                 motor_left_speed_pin: int, 
                 motor_left_direction_pin: int, 
                 motor_right_speed_pin: int,
-                motor_right_direction_pin: int
-                ):
+                motor_right_direction_pin: int,
+                speed: int = 0.3
+        ):
         self._motor_left = Motor(motor_left_speed_pin, motor_left_direction_pin)
         self._motor_right = Motor(motor_right_speed_pin, motor_right_direction_pin)
+        self._speed = speed
 
     def set_velocity(self, direction_vector: tuple[float, float]):
         x, y = direction_vector
         epsilon = 0.1
         if x < -epsilon:
-            self._motor_left.set_velocity(-1)
-            self._motor_right.set_velocity(-1)
+            self._motor_left.set_velocity(-self._speed)
+            self._motor_right.set_velocity(-self._speed)
         elif x > epsilon or y < 0:
-            self._motor_left.set_velocity(1)
-            self._motor_right.set_velocity(1)
+            self._motor_left.set_velocity(self._speed)
+            self._motor_right.set_velocity(self._speed)
         else:
-            self._motor_left.set_velocity(1)
-            self._motor_right.set_velocity(-1)
+            self._motor_left.set_velocity(self._speed)
+            self._motor_right.set_velocity(-self._speed)
 
     
     async def start_running(self):
