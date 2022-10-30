d1f6bc35f78e5b73c64a35c47f9b4ef039a97bd8
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-10-30 18:40:37 +0100

    temp

--- test_motor.py
+++ test_motor.py
@@ -4,9 +4,12 @@ import asyncio
 import signal
 import time
 import sys
+from simple_pid import PID
 
-curr_time = time.time()
-
+pid = PID(0.6, 5, 0, setpoint=50)
+pid.sample_time(0.1)
+pid.output_limits(0, 100)
+interrupt_count = 0
 def setup():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(32, GPIO.OUT)
@@ -21,19 +24,18 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 def interrupt(channel):
-    global curr_time
-    print(time.time() - curr_time)
-    curr_time = time.time()
+    global interrupt_count
+    interrupt_count += 1
 
 setup()
 motor_pwm = GPIO.PWM(32, 50)
-motor_pwm.start(0)
+motor_pwm.start(5)
 
 signal.signal(signal.SIGINT, signal_handler)
-for i in range(101):
-    motor_pwm.ChangeDutyCycle(i)
-    time.sleep(0.1)
-
+while True:
+    output = pid(interrupt_count)
+    motor_pwm.ChangeDutyCycle(output)
+    print(output)
 signal.pause()
 
 
