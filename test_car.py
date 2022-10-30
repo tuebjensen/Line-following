80a703f615d770f55228cde44db2590bb7764499
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-10-30 21:22:22 +0100

    temp

--- test_car.py
+++ test_car.py
@@ -16,10 +16,16 @@ car = Car(32, 36, 11, 33, 31, 37)
 #motor1 = Motor(speed_pin=32, direction_pin=36, encoder_interrupt_pin=11, speed=20, forwards=False)
 #motor2 = Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37, speed=20, forwards=True)
 async def main():
-    for i in range(50):
-        direction = cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
+    for i in range(3):
+        direction = (0.05, 0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
         car.set_velocity(direction)
-        await asyncio.sleep(0.1)
+        await asyncio.sleep(1)
+        direction = (-0.15, 0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
+        car.set_velocity(direction)
+        await asyncio.sleep(1)
+        direction = (-0.25, -0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
+        car.set_velocity(direction)
+        await asyncio.sleep(1)
 
 async def start():
     await asyncio.gather(
