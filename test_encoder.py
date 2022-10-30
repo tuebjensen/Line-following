58ed7f18ade0820bd298cc02b24a276f695b08c8
Tamas Halasi <trusted.tomato@gmail.com>
2022-10-30 22:03:40 +0100

    temp

--- test_encoder.py
+++ test_encoder.py
@@ -1,6 +1,15 @@
 from motor import Motor
 import asyncio
+import signal
+import sys
 
+def signal_handler(sig, frame):
+    GPIO.cleanup()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+GPIO.setmode(GPIO.BOARD)
 motor1 = Motor(speed_pin=32, direction_pin=36, encoder_interrupt_pin=11)
 motor2 = Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37)
 
