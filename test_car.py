89f24fc1b4d8e5542157fa7a1d108b41091efa6c
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-11-01 14:01:46 +0100

    temp

--- test_car.py
+++ test_car.py
@@ -14,8 +14,8 @@ signal.signal(signal.SIGINT, signal_handler)
 
 GPIO.setmode(GPIO.BOARD)
 car = Car(
-    motor_left=Motor(speed_pin=32, direction_pin=36, encoder_interrupt_pin=11),
-    motor_right=Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37),
+    motor_left=Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37),
+    motor_right=Motor(speed_pin=32, direction_pin=36, encoder_interrupt_pin=11),
     speed=20
 )
 async def main():
