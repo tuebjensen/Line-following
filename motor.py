3c10cb3efff1bf0da8eeb4779ae96359e96b0d47
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-11-01 13:38:06 +0100

    temp

--- motor.py
+++ motor.py
@@ -62,6 +62,7 @@ class Motor:
         try:
             # control the speed of the motor
             while True:
+                print("PID")
                 GPIO.output(self._direction_pin, self._forwards)
                 output = self._pid(self._encoder_interrupt_count)
                 self._encoder_interrupt_count = 0
