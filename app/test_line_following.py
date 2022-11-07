b14c88fbd2645889b2a57dfeb5780ab02f54fee9
Tamas Halasi <trusted.tomato@gmail.com>
2022-11-07 15:58:56 +0100

    use multiprocessing

--- app/test_line_following.py
+++ app/test_line_following.py
@@ -1,4 +1,6 @@
 import asyncio
+from functools import partial
+from concurrent.futures import ProcessPoolExecutor
 from math import asin, atan2, cos, pi, sin, sqrt
 from typing import Tuple
 import cv2 as cv
@@ -21,47 +23,47 @@ def signal_handler(sig, frame):
     GPIO.cleanup()
     sys.exit(0)
 
-signal.signal(signal.SIGINT, signal_handler)
-GPIO.setmode(GPIO.BOARD)
+async def process_frame(original_frame):
+    original_frame = original_frame[:, 30:]
+    #original_frame = cv.flip(original_frame, 1)
+    #original_frame = cv.rotate(original_frame, cv.ROTATE_90_CLOCKWISE)
+    if not ret_read:
+        cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+        return
+    
+    blur = 3#cv.getTrackbarPos('Blur', 'image')
+    block_size = 5#cv.getTrackbarPos('Block size', 'image')
+    c = 3#cv.getTrackbarPos('C', 'image')
+    processed_frame = process_frame(original_frame, blur, block_size, c)
+    edges, houghlines = find_edges_and_lines(processed_frame)
+    if isinstance(houghlines, np.ndarray):
+        lines = get_from_houghlines(houghlines)
+        merged_lines = merge_lines(lines)
+        parallel_line_centers = get_centers_of_parallel_line_pairs(merged_lines)
+        display_all_lines(lines, original_frame)
+        display_merged_parallel_lines(merged_lines, original_frame)
+        display_center_of_parallel_lines(parallel_line_centers, original_frame)
+        display_displacement_and_direction_vectors(parallel_line_centers, original_frame)
+        display_direction_to_go(parallel_line_centers, original_frame)
+        
+        if parallel_line_centers is not None and len(parallel_line_centers) > 0:
+            velocity_vector = get_direction_to_go(parallel_line_centers[0], original_frame)
+            direction = velocity_vector.y, velocity_vector.x
+            print(str(direction))
+            car.set_velocity(direction)
+    
+    ret_encode, buffer = cv.imencode('.jpg', original_frame)
+    frame_encoded = buffer.tobytes()
+    video.set_frame_encoded(frame_encoded)
 
 async def process_video():
-    while cap.isOpened():
-        await asyncio.sleep(0.1)
-
-        ret_read, original_frame = cap.read()
-        original_frame = original_frame[:, 30:]
-        #original_frame = cv.flip(original_frame, 1)
-        #original_frame = cv.rotate(original_frame, cv.ROTATE_90_CLOCKWISE)
-        if not ret_read:
-            cap.set(cv.CAP_PROP_POS_FRAMES, 0)
-            continue
-        
-        blur = 3#cv.getTrackbarPos('Blur', 'image')
-        block_size = 5#cv.getTrackbarPos('Block size', 'image')
-        c = 3#cv.getTrackbarPos('C', 'image')
-        processed_frame = process_frame(original_frame, blur, block_size, c)
-        edges, houghlines = find_edges_and_lines(processed_frame)
-        if isinstance(houghlines, np.ndarray):
-            lines = get_from_houghlines(houghlines)
-            merged_lines = merge_lines(lines)
-            parallel_line_centers = get_centers_of_parallel_line_pairs(merged_lines)
-            display_all_lines(lines, original_frame)
-            display_merged_parallel_lines(merged_lines, original_frame)
-            display_center_of_parallel_lines(parallel_line_centers, original_frame)
-            display_displacement_and_direction_vectors(parallel_line_centers, original_frame)
-            display_direction_to_go(parallel_line_centers, original_frame)
-            
-            if parallel_line_centers is not None and len(parallel_line_centers) > 0:
-                velocity_vector = get_direction_to_go(parallel_line_centers[0], original_frame)
-                direction = velocity_vector.y, velocity_vector.x
-                print(str(direction))
-                car.set_velocity(direction)
-        
-        ret_encode, buffer = cv.imencode('.jpg', original_frame)
-        frame_encoded = buffer.tobytes()
-        video.set_frame_encoded(frame_encoded)
-
-    cap.release()
+    with ProcessPoolExecutor() as executor:
+        loop = asyncio.get_running_loop()
+        executor = ProcessPoolExecutor()
+        while cap.isOpened():
+            ret_read, original_frame = cap.read()
+            await loop.run_in_executor(executor, partial(process_frame, original_frame))
+        cap.release()
 
 async def start():
     await asyncio.gather(
@@ -71,6 +73,9 @@ async def start():
     )
 
 if __name__ == "__main__":
+    signal.signal(signal.SIGINT, signal_handler)
+    GPIO.setmode(GPIO.BOARD)
+
     cap = cv.VideoCapture(0)
     car = Car(
         motor_left=Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37),
