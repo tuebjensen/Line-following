8abcc565c221be142783e4d2d7a51ce6fb00aca5
Tamas Halasi <trusted.tomato@gmail.com>
2022-11-09 18:58:45 +0100

    temp

--- app/run_encoder.py
+++ app/run_encoder.py
@@ -2,23 +2,27 @@ from lib_motor import Motor
 import asyncio
 import sys
 import RPi.GPIO as GPIO
-from time import sleep
-
+from time import time
 encoder_interrupt_pin = int(sys.argv[1])#[int(e) for e in sys.argv[1:2]]
 count = 0
-start = time()
-last_val = False
-
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(encoder_interrupt_pin, GPIO.IN)
-
-def callback ():
-    global count
-    count += 1
-
-GPIO.add_event_detect(encoder_interrupt_pin, GPIO.FALLING, callback=callback)
+start = time()
 
 while True:
-    sleep(0.1)
-    print(count, flush=True)
-    count = 0
+    now = time()
+    timeout = int(100 - (now - start) * 1000)
+    
+    if timeout <= 0:
+        start = now
+        count = 0
+        continue
+
+    ret_wait = GPIO.wait_for_edge(encoder_interrupt_pin, GPIO.FALLING, timeout = 100)
+    
+    if ret_wait is not None:
+        count += 1
+    else:
+        print(str(count))
+        start = now
+        count = 0
