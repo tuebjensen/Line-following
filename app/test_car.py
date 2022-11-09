87b180dd54c6cbfeaf88b1815c1870e1820b93d7
Tamas Halasi <trusted.tomato@gmail.com>
2022-11-09 19:01:27 +0100

    temp

--- app/test_car.py
+++ app/test_car.py
@@ -16,11 +16,17 @@ GPIO.setmode(GPIO.BOARD)
 car = Car(
     motor_left=Motor(speed_pin=33, direction_pin=31, encoder_interrupt_pin=37),
     motor_right=Motor(speed_pin=32, direction_pin=36, encoder_interrupt_pin=11),
-    speed=40
+    speed=20
 )
 async def main():
-    for i in range(10):
+    for i in range(3):
         direction = (0.05, 0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
+        car.set_velocity(direction)
+        await asyncio.sleep(3)
+        direction = (-0.15, 0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
+        car.set_velocity(direction)
+        await asyncio.sleep(3)
+        direction = (-0.05, -0.9)#cos(2 * pi / 50 * i), sin(2 * pi / 50 * i)
         car.set_velocity(direction)
         await asyncio.sleep(3)
 
