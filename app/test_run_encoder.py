d422c13a3cf0f305f8d62d17addea9f975537dbe
Tamas Halasi <trusted.tomato@gmail.com>
2022-11-09 17:07:34 +0100

    temp

--- app/test_run_encoder.py
+++ app/test_run_encoder.py
@@ -4,14 +4,14 @@ import sys
 async def run_encoder ():
     encoder_process = await asyncio.create_subprocess_exec(
         sys.executable, 'run_encoder.py', '11',
-        stdout = None,
+        stdout = asyncio.subprocess.PIPE,
         stderr = asyncio.subprocess.DEVNULL,
-        stdin=asyncio.subprocess.DEVNULL
+        stdin = asyncio.subprocess.DEVNULL
     )
     while True:
         print('hello')
         try:
-            line = await asyncio.wait_for(encoder_process.stdout.read(1), 1)
+            line = await asyncio.wait_for(encoder_process.stdout.readline(), 1)
         except asyncio.TimeoutError:
             pass
         print('hoy')
