22e6b3313f6b6c346d0142a2473bc347a8c47862
Norbert <garrote713@gmail.com>
2022-11-09 16:36:59 +0100

    merged line segments

--- app/lib_process_lines.py
+++ app/lib_process_lines.py
@@ -1,6 +1,11 @@
+from math import cos, pi, sin, tan
 from statistics import median
 from typing import Union
 
+BOX_SIZE= 20
+PIXELS_THRESHOLD = 2*BOX_SIZE*0.75
+MIN_LINE_SEGMENT_SIZE = 4
+MIN_LINE_SEGMENT_HOLE_SIZE = 4
 
 class Line:
     _rho_diff_threshold = 80.0
@@ -14,6 +19,10 @@ class Line:
         return (abs(self.rho - to_compare.rho) < self._rho_diff_threshold 
                 and abs(self.theta - to_compare.theta) < self._theta_diff_threshold)
 
+class LineSegment:
+    def __init__(self, start_point: 'tuple[int, int]', end_point: 'tuple[int, int]') -> 'LineSegment':
+        self.start_point = start_point
+        self.end_point = end_point
 
 
 def get_from_houghlines(hough_lines) -> 'list[Line]':
@@ -44,6 +53,133 @@ def merge_lines(lines: 'list[Line]') -> 'list[Line]':
     return merged_lines
 
 
+def get_tape_corner_line_segments_please(merged_lines: 'list[Line]', edges) -> 'dict[Line, list[LineSegment]]':
+    lines_segments: dict[Line, list[LineSegment]] = {}
+    for line in merged_lines:
+        box_centers = _get_box_centers_please(line, edges)
+        white_pixels_per_box = _get_white_pixels_per_box_please(box_centers, edges)
+        tape_markers = _get_tape_markers_please(white_pixels_per_box)
+        filtered_markers = _get_filtered_markers_please(tape_markers)
+        lines_segments[line] = _get_line_segments_please(filtered_markers, box_centers)
+    return lines_segments
+
+def _get_box_centers_please(line:'Line', edges) -> 'list[tuple[int,int]]':
+    box_centers = []
+    max_x = edges.shape[1]
+    max_y = edges.shape[0]
+    rho, theta = line.rho, line.theta
+    iterate_along_x_axis = _is_line_more_horizontal_please(line)
+    delta_x = BOX_SIZE if iterate_along_x_axis else BOX_SIZE * 1/tan(theta - pi/2)
+    delta_y = BOX_SIZE if not iterate_along_x_axis else BOX_SIZE * tan(theta - pi/2)
+    x_intercept = rho*cos(theta) - rho*sin(theta)/tan(theta - pi/2)
+    y_intercept = rho*sin(theta) - rho*cos(theta)*tan(theta - pi/2)
+
+    box_count = int(max_x / BOX_SIZE if iterate_along_x_axis else max_y / BOX_SIZE)
+
+    for i in range(box_count):
+        x = i * delta_x if iterate_along_x_axis else round(x_intercept + i*delta_x)
+        y = i * delta_y if not iterate_along_x_axis else round(y_intercept + i*delta_y)
+        if y >= 0 and y < max_y and x >= 0 and x <= max_x:
+            box_centers.append((x, y))
+    return box_centers
+
+def _is_line_more_horizontal_please(line:'Line') -> bool:
+    return line.theta >= pi/4 and line.theta <= 3*pi/4
+
+def _get_white_pixels_per_box_please(box_centers: 'list[tuple[int,int]]', edges) -> 'list[int]':
+    max_x = edges.shape[1]
+    max_y = edges.shape[0]
+    pixels_per_box = []
+    half_box_size = int(BOX_SIZE / 2)
+    for (x, y) in box_centers:
+        pixels_count = 0
+        for delta_x in range(-half_box_size, half_box_size):
+            for delta_y in range(-half_box_size, half_box_size):
+                if (x + delta_x > 0 and x + delta_x < max_x
+                        and y + delta_y > 0 and y + delta_y < max_y
+                        and edges[y + delta_y, x + delta_x] == 255):
+                    pixels_count += 1
+        pixels_per_box.append(pixels_count)
+    return pixels_per_box
+
+def _get_tape_markers_please(white_pixels_per_box: 'list[int]'):
+    return [pixel_count > PIXELS_THRESHOLD for pixel_count in white_pixels_per_box]
+
+
+def _get_filtered_markers_please(tape_markers):
+    filtered_markers = _fill_gaps(tape_markers)
+    filtered_markers = _filter_small_segments(filtered_markers)
+    return filtered_markers
+
+def _fill_gaps(tape_markers):
+    look_forward_amount = MIN_LINE_SEGMENT_HOLE_SIZE - 1
+    gap_filled_markers = tape_markers.copy()
+    recording = False
+    current_segment_size = 0
+    i = -1
+    while i < len(tape_markers) - 1:
+        i += 1
+        if tape_markers[i] is True:
+            recording = True
+            current_segment_size += 1
+        elif recording is True and tape_markers[i] is False:
+            markers_forward = 0
+            end_index = i
+            for j in range(1, look_forward_amount + 1):
+                if (i + j < len(tape_markers) - 1
+                        and tape_markers[i + j] == True):
+                    markers_forward += 1
+                    end_index = i + j
+            if markers_forward == 0:
+                i += look_forward_amount
+                current_segment_size = 0
+                recording = False
+            elif current_segment_size + markers_forward + 1 >= MIN_LINE_SEGMENT_HOLE_SIZE:
+                gap_filled_markers[i : end_index + 1] = [True] * (end_index - i + 1)
+                current_segment_size += end_index - i
+                i = end_index - 1 # -1 because incrementing is the first step of the loop
+    return gap_filled_markers
+
+def _filter_small_segments(gap_filled_markers):
+    filtered_markers = gap_filled_markers.copy()
+    i = 0
+    while i < len(gap_filled_markers) - 1:
+        start, end = _get_next_segments_indices(gap_filled_markers, i)
+        _filter_segment_if_too_small(filtered_markers, start, end)
+        i = i+1 if end is None else end
+    return filtered_markers
+
+def _get_next_segments_indices(markers: 'list[bool]', start_from: int) -> 'tuple[int, int]':
+    segment_started = False
+    start_index = None
+    end_index = None
+    for i in range(start_from, len(markers)):
+        if segment_started is False and markers[i] is True:
+            segment_started = True
+            start_index = i
+        if segment_started and markers[i] is False:
+            segment_started = False
+            end_index = i
+            break
+    return start_index, end_index or (None if start_index is None else len(markers) - 1)
+
+def _filter_segment_if_too_small(markers, segment_start, segment_end):
+    if segment_start != None and segment_end - segment_start < MIN_LINE_SEGMENT_SIZE:
+        markers[segment_start : segment_end + 1] = [False] * (segment_end - segment_start + 1)
+
+def _get_line_segments_please(markers, centers):
+    line_segments = []
+    search_from = 0
+    while search_from is not None:
+        start, end = _get_next_segments_indices(markers, search_from)
+        if start is not None:
+            start_x, start_y = centers[start]
+            end_x, end_y = centers[end]
+            line_segments.append(LineSegment((start_x, start_y), (end_x, end_y)))
+        search_from = None if end is None else end + 1
+    return line_segments
+
+
 def _get_median_line(lines: 'list[Line]') -> 'Line':
     rhos = []
     thetas = []
@@ -71,9 +207,9 @@ def get_centers_of_parallel_line_pairs(lines: 'list[Line]') -> 'list[Line]':
 
     center_lines: list[Line] = []
     for same_angle_lines in list(parallel_lines.values()):
-        if(len(same_angle_lines) != 2):
-            continue
-        center_lines.append(_get_center_line(same_angle_lines[0], same_angle_lines[1]))
+        if(len(same_angle_lines) == 2):
+            center_lines.append(_get_center_line(same_angle_lines[0], same_angle_lines[1]))
+
     return center_lines
 
 
---
+++ app/test_line_processing.py
@@ -0,0 +1,168 @@
+import cv2 as cv
+import numpy as np
+from typing import Tuple
+from lib_process_lines import Line, BOX_SIZE, _get_box_centers_please, get_centers_of_parallel_line_pairs, get_from_houghlines, get_tape_corner_line_segments_please, merge_lines
+from lib_calculate_direction import get_direction_to_go, get_direction_vector_of_line, get_displacement_vector_from_center
+
+
+
+def nothing(x):
+    pass
+
+def process_frame(frame, blur, block_size, c):
+    processed_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    processed_frame = cv.medianBlur(processed_frame, 2*blur+1)
+    processed_frame = cv.adaptiveThreshold(processed_frame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 2*(block_size+1)+1, c)
+    return processed_frame
+
+def find_edges_and_lines(frame):
+    edges = cv.Canny(frame, 50, 150, apertureSize = 3)
+    lines = cv.HoughLines(edges, 1, np.pi/180, 100)
+    return edges, lines
+
+def put_line_on_frame(frame, line: Line, color: Tuple[int, int, int]):
+    rho, theta = line.rho, line.theta
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a*rho
+    y0 = b*rho
+    x1 = int(x0 + 1000*(-b))
+    y1 = int(y0 + 1000*(a))
+    x2 = int(x0 - 1000*(-b))
+    y2 = int(y0 - 1000*(a))
+    cv.line(frame, (x1,y1), (x2,y2), color, 2)
+
+def display_boxes_around_merged_lines(merged_lines: 'list[Line]', frame, edges):
+    for i in range(len(merged_lines)):
+        line = merged_lines[i]
+        color = (0, 255-255/len(merged_lines)*i ,0) # same shade of green as the merged line
+        boxes = _get_box_centers_please(line, edges)
+        for box in boxes:
+            x, y = box
+            x = int(x)
+            y = int(y)
+            half_box_size = int(BOX_SIZE/2)
+            cv.line(frame, (x - half_box_size, y - half_box_size), (x + half_box_size, y - half_box_size), color, 1)
+            cv.line(frame, (x - half_box_size, y - half_box_size), (x - half_box_size, y + half_box_size), color, 1)
+            cv.line(frame, (x + half_box_size, y + half_box_size), (x + half_box_size, y - half_box_size), color, 1)
+            cv.line(frame, (x + half_box_size, y + half_box_size), (x - half_box_size, y + half_box_size), color, 1)
+            # putting it on the contour image
+            # cv.line(edges, (x - half_box_size, y - half_box_size), (x + half_box_size, y - half_box_size), (255, 255, 255), 1)
+            # cv.line(edges, (x - half_box_size, y - half_box_size), (x - half_box_size, y + half_box_size), (255, 255, 255), 1)
+            # cv.line(edges, (x + half_box_size, y + half_box_size), (x + half_box_size, y - half_box_size), (255, 255, 255), 1)
+            # cv.line(edges, (x + half_box_size, y + half_box_size), (x - half_box_size, y + half_box_size), (255, 255, 255), 1)
+
+def display_merged_lines_segments(merged_lines_segments, frame):
+    merged_lines = list(merged_lines_segments.keys())
+    for i in range(len(merged_lines)):
+        line_segments = merged_lines_segments[merged_lines[i]]
+        for j in range(len(line_segments)):
+            color = ((255-255/len(merged_lines)*i, 255-255/len(merged_lines)*i,0))
+            cv.line(frame, (line_segments[j].start_point[0],line_segments[j].start_point[1]), (line_segments[j].end_point[0],line_segments[j].end_point[1]), color, 2)
+
+
+def display_all_lines(lines: 'list[Line]', frame):
+    for line in lines:
+        color = (255, 0, 0) # blue (BGR)
+        put_line_on_frame(frame, line, color)
+
+def display_merged_parallel_lines(merged_lines: 'list[Line]', frame):
+    for i in range(len(merged_lines)):
+        line = merged_lines[i]
+        color = (0, 255-255/len(merged_lines)*i ,0) # shade of green
+        put_line_on_frame(frame, line, color)
+
+def display_center_of_parallel_lines(parallel_line_centers, frame):
+    if parallel_line_centers is None:
+        return
+    for i in range(len(parallel_line_centers)):
+        line = parallel_line_centers[i]
+        color = (0, 0, 255-255/len(parallel_line_centers)*i) # shade of red
+        put_line_on_frame(frame, line, color)
+
+def display_displacement_and_direction_vectors(parallel_line_centers, frame):
+    if(not isinstance(parallel_line_centers, (list, tuple, np.ndarray))):
+        return
+    if(len(parallel_line_centers) < 1):
+        return
+    line = parallel_line_centers[0]
+    displacement_vector = get_displacement_vector_from_center(line, frame)
+    frame_width = frame.shape[1]
+    frame_height = frame.shape[0]
+    center_x = frame_width / 2
+    center_y = frame_height / 2
+    cv.line(frame, (int(center_x), int(center_y)), (int(displacement_vector.x) + int(center_x), int(center_y) - int(displacement_vector.y)), (255,255,0), 2)
+    direction_vector = get_direction_vector_of_line(line) * 200
+    cv.line(frame,
+        (int(displacement_vector.x) + int(center_x), int(center_y) - int(displacement_vector.y)),
+        (int(displacement_vector.x) + int(direction_vector.x) + int(center_x), int(center_y) - int(direction_vector.y) - int(displacement_vector.y)),
+        (0,255,255),
+        2
+    )
+
+def display_direction_to_go(parallel_line_centers, frame):
+    if(not isinstance(parallel_line_centers, (list, tuple, np.ndarray))):
+        return
+    if(len(parallel_line_centers) < 1):
+        return
+    line = parallel_line_centers[0]
+    frame_width = frame.shape[1]
+    frame_height = frame.shape[0]
+    center_x = frame_width / 2
+    center_y = frame_height / 2
+    direction_to_go = get_direction_to_go(line, frame) * 50
+    cv.line(frame, (int(center_x), int(center_y)), (int(direction_to_go.x) + int(center_x), int(center_y) - int(direction_to_go.y)), (0,69,255), 2)
+
+
+def process_video():
+    guard = True
+    cap = cv.VideoCapture(0)
+    img = np.zeros((25, 500, 3), np.uint8)
+    cv.namedWindow('image')
+    cv.createTrackbar('Blur', 'image', 5, 100, nothing)
+    cv.createTrackbar('Block size', 'image', 5, 100, nothing)
+    cv.createTrackbar('C', 'image', 5, 100, nothing)
+    while cap.isOpened() and guard:
+        ret, original_frame = cap.read()
+        original_frame = cv.flip(original_frame, 1)
+        original_frame = cv.rotate(original_frame, cv.ROTATE_90_CLOCKWISE)
+        if not ret:
+            print("Can't receive next frame")
+            cap.set(cv.CAP_PROP_POS_FRAMES, 0)
+            continue
+        
+        blur = cv.getTrackbarPos('Blur', 'image')
+        block_size = cv.getTrackbarPos('Block size', 'image')
+        c = cv.getTrackbarPos('C', 'image')
+        processed_frame = process_frame(original_frame, blur, block_size, c)
+        edges, houghlines = find_edges_and_lines(processed_frame)
+
+        if isinstance(houghlines, np.ndarray):
+            cv.putText(original_frame, f'lines: {len(houghlines)}', (0,50), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 2, cv.LINE_AA)
+
+            lines = get_from_houghlines(houghlines)
+            merged_lines = merge_lines(lines)
+            merged_lines_segments = get_tape_corner_line_segments_please(merged_lines, edges)
+            parallel_line_centers = get_centers_of_parallel_line_pairs(merged_lines)
+            display_all_lines(lines, original_frame)
+            display_merged_parallel_lines(merged_lines, original_frame)
+            display_boxes_around_merged_lines(merged_lines, original_frame, edges)
+            display_merged_lines_segments(merged_lines_segments, original_frame)
+            display_center_of_parallel_lines(parallel_line_centers, original_frame)
+            display_displacement_and_direction_vectors(parallel_line_centers, original_frame)
+            display_direction_to_go(parallel_line_centers, original_frame)
+
+        cv.imshow('original video', original_frame)
+        cv.imshow('processed video', processed_frame)
+        cv.imshow('edges', edges)
+        cv.imshow('image', img)
+        cv.moveWindow('processed video', 700, 208)
+        cv.moveWindow('edges', 1200, 208)
+        if cv.waitKey(10) == ord('q'):
+            guard = False
+    cap.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    process_video()
