b6e1b4911c6cea8ee1bc6599ca340cbc41bc59f2
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-11-22 21:59:06 +0100

    add functionality to web server

--- app/lib_web_server.py
+++ app/lib_web_server.py
@@ -18,10 +18,27 @@ class WebServer:
         self._frame_encoded = frame_encoded
         self._is_frame_encoded_changed = True
 
+    async def send_message(self, type, data):
+        message = {
+            'type': type,
+            'data': data,
+            'processedIds': self._processed_ids        
+        }
+
+        self._ws.send_str(json.dumps(message))
+        
+        self._processed_ids.clear()
+
+    async def set_current_node(self, current_node):
+        async with aiofiles.open('server_state.json', 'r') as file:
+            await file.write(json.dumps({'currentNode': current_node}))
+        self.send_message('server-state-update', {'currentNode': current_node})
+
     async def start_running(self, address, port, path_callback):
         if self._is_running:
             return
         self._is_running = True
+
 
         async def websocket_test(request):
             return web.FileResponse('website/websocket.html')
@@ -35,44 +52,56 @@ class WebServer:
             async with aiofiles.open('client_state.json', 'r') as file:
                 client_state = json.loads(await file.read())
 
-            full_state = {
-                "type": "full-state-update", 
-                "data": {
-                    "clientState": client_state, 
-                    "serverState": server_state
-                }
+            full_state = { 
+                "clientState": client_state, 
+                "serverState": server_state
             }
 
             return full_state
+
+        async def set_server_state(server_state):
+            async with aiofiles.open('server_state.json', 'r') as file:
+                await file.write(json.dumps({server_state}))
+
+        async def set_client_state(client_state):
+            async with aiofiles.open('client_state.json', 'w') as file:
+                await file.write(json.dumps(client_state))
+            path_callback(client_state['path'])
+
+        async def update_client_state(client_state):
+            async with aiofiles.open('client-state.json', 'wr') as file:
+                old_client_state = json.loads(await file.read())
+                new_client_state = old_client_state | client_state
+                await file.write(json.dumps(new_client_state))
+            if 'path' in client_state:
+                path_callback(client_state['path'])
             
         async def websocket_handler(request):   
-            if self._websocket_lock:
+            if self._ws is not None:
                 raise web.HTTPConflict()
-            self._websocket_lock = True
-            ws = web.WebSocketResponse()
-
-            await ws.prepare(request)
+            self._ws = web.WebSocketResponse()
+            self._processed_ids = []
+            await self._ws.prepare(request)
             #async with aiofiles.open('state.json', 'r') as file:
             #    contents = await file.read()
             contents = await make_full_state()
-            await ws.send_str(json.dumps(contents)) 
-        
-            async for msg in ws:
+            await self.send_message('full-state-update', contents) 
+
+            async for msg in self._ws:
                 if msg.type == aiohttp.WSMsgType.TEXT:
                     message = json.loads(msg.data)
                     data = message['data']
+                    if 'id' in message:
+                        self._processed_ids.append(message['id'])
                     if message['type'] == 'server-state-update':
-                        async with aiofiles.open('server_state.json', 'w') as file:
-                            await file.write(json.dumps(data))
-                        
-                        # await ws.send_str(response)
+                        set_server_state(data)
                     elif message['type'] == 'client-state-update':
-                        async with aiofiles.open('client_state.json', 'w') as file:
-                            await file.write(json.dumps(data))
+                        set_client_state(data)
                         
-                        path_callback(data['path'])
-            self._websocket_lock = False
-            return ws
+            self._ws = None
+            self._processed_ids.clear()
+            update_client_state({'path': []})
+            return self._ws
 
 
         async def show_image(request):
@@ -100,7 +129,7 @@ class WebServer:
         app.router.add_route('GET', '/websocket', websocket_test)
         app.router.add_route('GET', '/', index)
         app.router.add_route('GET', '/video', show_image)
-        app.router.add_routes([web.get('/ws', websocket_handler)])
+        app.router.add_routes([web.get('/self._ws', websocket_handler)])
         app.add_routes([web.static('/assets', 'website/assets')])
         
 
