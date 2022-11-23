f37e62ec41eef92331e70cda3164abd983fb7466
Norbert_Pap <pap.norbert98@gmail.com>
2022-11-23 15:37:15 +0100

    temp

--- app/lib_car.py
+++ app/lib_car.py
@@ -16,29 +16,35 @@ class Car:
         if direction_vector is None:
             return
         x, y = direction_vector
-        speed_factor = 1 - atan2(y, abs(x)) / (pi / 2) # TODO: Revise: y<0 means speed_factor > 1
-        epsilon = 0.1
-        if (x, y) == (0, 0):
-            self._motor_left.set_speed(0)
-            self._motor_right.set_speed(0)
-        #Turn left
-        elif x < -epsilon: 
-            self._motor_left.set_speed(self._speed * speed_factor)
-            self._motor_right.set_speed(self._speed * speed_factor)
-            self._motor_left.set_forwards(False)
-            self._motor_right.set_forwards(True)
-        #Turn right
-        elif x > epsilon or y < 0:
-            self._motor_left.set_speed(self._speed * speed_factor)
-            self._motor_right.set_speed(self._speed * speed_factor)
-            self._motor_left.set_forwards(True)
-            self._motor_right.set_forwards(False)
-        #Go straight
-        else:
-            self._motor_left.set_speed(self._speed)
-            self._motor_right.set_speed(self._speed)
-            self._motor_left.set_forwards(True)
-            self._motor_right.set_forwards(True)
+        # speed_factor = 1 - atan2(y, abs(x)) / (pi / 2) # TODO: Revise: y<0 means speed_factor > 1
+        # epsilon = 0.1
+        # if (x, y) == (0, 0):
+        #     self._motor_left.set_speed(0)
+        #     self._motor_right.set_speed(0)
+        # #Turn left
+        # elif x < -epsilon: 
+        #     self._motor_left.set_speed(self._speed * speed_factor)
+        #     self._motor_right.set_speed(self._speed * speed_factor)
+        #     self._motor_left.set_forwards(False)
+        #     self._motor_right.set_forwards(True)
+        # #Turn right
+        # elif x > epsilon or y < 0:
+        #     self._motor_left.set_speed(self._speed * speed_factor)
+        #     self._motor_right.set_speed(self._speed * speed_factor)
+        #     self._motor_left.set_forwards(True)
+        #     self._motor_right.set_forwards(False)
+        # #Go straight
+        # else:
+        #     self._motor_left.set_speed(self._speed)
+        #     self._motor_right.set_speed(self._speed)
+        #     self._motor_left.set_forwards(True)
+        #     self._motor_right.set_forwards(True)
+        left_motor_intensity = y + x
+        right_motor_intensity = y - x
+        self._motor_left.set_speed(abs(self._speed * left_motor_intensity))
+        self._motor_left.set_forwards(left_motor_intensity >= 0)
+        self._motor_right.set_speed(abs(self._speed * right_motor_intensity))
+        self._motor_right.set_forwards(right_motor_intensity >= 0)
 
     
     async def start_running(self):
