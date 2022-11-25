ef25de1e7ad4db7541dbfccc2cde8aa53268300b
Tue Burmeister Jensen <tuebjensen@gmail.com>
2022-11-25 18:38:37 +0100

    temp

--- app/test_video_streaming.py
+++ app/test_video_streaming.py
@@ -1,10 +1,15 @@
 import cv2 as cv
+from lib_calculate_direction import DirectionCalculator
+from lib_image_processing import ImageProcessor
+from lib_process_lines import LineProcessor
 from test_line_processing import get_processed_frame
 from lib_web_server import WebServer
 import asyncio
 camera = cv.VideoCapture(0)
 video = WebServer()
-
+image_processor = ImageProcessor()
+line_processor = LineProcessor()
+direction_calculator = DirectionCalculator(video)
 
 async def main():
     while camera.isOpened():
