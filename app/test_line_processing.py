61ec5f7dec804f9d8c947dd1dbb71e4a900f4fda
Norbert_Pap <pap.norbert98@gmail.com>
2022-11-27 14:08:20 +0100

    temp

--- app/test_line_processing.py
+++ app/test_line_processing.py
@@ -43,23 +43,24 @@ def get_processed_frame(original_frame,
         merged_lines = line_processor._merge_lines(lines, original_frame)
         merged_lines_segments = line_processor._get_tape_boundaries(merged_lines, edges)
         parallel_line_centers = line_processor._get_centers_of_parallel_line_pairs(merged_lines)
-        tape_paths_and_lines = line_processor._get_tape_paths_and_lines(parallel_line_centers, merged_lines_segments, original_frame)
+        tape_paths = line_processor._get_tape_paths_and_lines(parallel_line_centers, merged_lines_segments, original_frame)
         display_all_lines(lines, original_frame)
         display_merged_parallel_lines(merged_lines, original_frame)
         display_boxes_around_merged_lines(merged_lines, original_frame, edges)
         display_merged_lines_segments(merged_lines_segments, original_frame)
         display_center_of_parallel_lines(parallel_line_centers, original_frame)
-        display_tape_paths(tape_paths_and_lines, original_frame)
+        display_tape_paths(tape_paths, original_frame)
 
-        target_segment, target_line, current_node = direction_calculator._decide_target(
-                                                                original_frame,
-                                                                tape_paths_and_lines)
-        if target_segment is not None:
-            displacement_vector = direction_calculator._get_displacement_vector_from_center(target_line, original_frame)
-            direction_vector = target_segment.get_direction_vector_please()
-            velocity_vector = direction_calculator._get_direction_to_go(displacement_vector, direction_vector, original_frame)
-            display_displacement_and_direction_vectors(displacement_vector, direction_vector, original_frame)
-            display_direction_to_go(velocity_vector, original_frame)
+    target_segment, target_line, current_node = direction_calculator._decide_target(
+                                                            original_frame,
+                                                            tape_paths)
+    if target_segment is not None:
+        displacement_vector = direction_calculator._get_displacement_vector_from_center(target_line, original_frame)
+        direction_vector = target_segment.get_direction_vector_please()
+        velocity_vector = direction_calculator._get_direction_to_go(displacement_vector, direction_vector, original_frame)
+        display_displacement_and_direction_vectors(displacement_vector, direction_vector, original_frame)
+        display_direction_to_go(velocity_vector, original_frame)
+
     cv.putText(original_frame, f'Frame: #{frames}, fps: {frames / (time.time() - start)}', (0,50), cv.FONT_HERSHEY_SIMPLEX, 1, (0,69,255), 2, cv.LINE_AA)
     cv.putText(original_frame, f'Stable: {_get_state_string(direction_calculator._stable_state)}', (0,80), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv.LINE_AA)
     cv.putText(original_frame, f'Incoming: {_get_state_string(direction_calculator._last_incoming_state)} x{direction_calculator._same_incoming_states_count}', (0,110), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2, cv.LINE_AA)
