fed5c330ef519da72c5833e5b1b2cc8d37f583ff
Norbert_Pap <pap.norbert98@gmail.com>
2022-12-05 16:17:15 +0100

    temp

--- app/test_video_streaming.py
+++ app/test_video_streaming.py
@@ -2,7 +2,7 @@ import cv2 as cv
 from lib_calculate_direction import DirectionCalculator
 from lib_image_processing import ImageProcessor
 from lib_process_lines import LineProcessor
-from test_line_processing import get_processed_frame_and_direction_vector
+from lib_line_following import get_processed_frame_and_direction_vector
 from lib_web_server import WebServer
 import asyncio
 camera = cv.VideoCapture(0)
